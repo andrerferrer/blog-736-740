b7a37f742ec454358dbfaa709fd16df4270b7a5c
Andre Ferrer <andrerferrer@hotmail.com>
2021-11-12 10:28:11 +0100

    Finish lovecode

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# download the picture from somewhere to the computer
+picture_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg'
+file = URI.open(picture_url)
+
+new_article = Article.create!(
+  title: 'Nintendo Entertainment System',
+  body: 'A very cool videogame'
+)
+# upload the picture to cloudiary, attaching to the article
+new_article.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
